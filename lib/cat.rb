9509920b49f1bef076ab8cc42d3734086de7a4f5
isaac <oseasisaac91@gmail.com>
2018-11-24 19:30:49 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 class Cat
   # code goes here
+  attr_reader :name
+
+  attr_accessor :mood
+
+  def initialize (name)
+    @name = name
+    self.mood = "nervous"
+  
+  end
+
 end
